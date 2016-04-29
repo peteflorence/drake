6929e14e8f4be169b6f730497bf5874816d10c87
Russ Tedrake <russt@mit.edu>
2016-04-29 07:00:36 -0400

    first pass

---
+++ drake/examples/Pendulum/fitValueFunction.m
@@ -0,0 +1,59 @@
+%function fitValueFunction
+
+load pendulum_value_iteration.mat;
+
+num_x = 2;
+Sgrid = cell(1,num_x);
+[Sgrid{:}] = ndgrid(xbins{:});
+% Sgrid{j}(i_1,i_2,...,i_{num_x}) = xbins{j}(i_j)
+S = cellfun(@(a)reshape(a,1,[]),Sgrid,'UniformOutput',false);
+S = vertcat(S{:});
+clear Sgrid;
+
+basis_generators = { @(x)sin(x(1,:)), @(x)cos(x(1,:)), @(x)x(2,:) };
+degrees = { 0:2, 0:2, 0:2 };
+degree_grid = cell(1,length(degrees));
+[degree_grid{:}] = ndgrid(degrees{:});
+degree = cellfun(@(a)reshape(a,1,[]),degree_grid,'UniformOutput',false);
+degree = vertcat(degree{:});
+clear degree_grid;
+
+phi = ones(size(degree,2),size(S,2));
+for i=1:size(degree,2)
+  for j=1:length(basis_generators)
+    phi(i,:) = phi(i,:).*(basis_generators{j}(S).^degree(j,i));
+  end
+end
+
+V1_indices = (J<30)';
+V2_indices = ~V1_indices;
+
+%% EM-style algorithm to fit two Lyapunov functions
+
+while (1)
+  % least squares fit to data J(V1_indices) ~= alpha1'*phi(:,V1_indices)
+  alpha1 = (phi(:,V1_indices)')\J(V1_indices);
+  alpha2 = (phi(:,V2_indices)')\J(V2_indices);
+  
+  V1 = alpha1'*phi(:,V1_indices);
+  V2 = alpha2'*phi(:,V2_indices);
+  
+  clf; hold on;
+  plot3(S(1,V1_indices),S(2,V1_indices),J(V1_indices),'r.');
+  plot3(S(1,V2_indices),S(2,V2_indices),J(V2_indices),'b.');
+  
+  plot3(S(1,V1_indices),S(2,V1_indices),V1,'g.');
+  plot3(S(1,V2_indices),S(2,V2_indices),V2,'m.');
+  axis tight; drawnow;
+  
+%  V1_indices_next = (J'-alpha1'*phi).^2 < (J'-alpha2'*phi).^2;
+  V1_indices_next = alpha1'*phi < alpha2'*phi;
+  
+  if all(V1_indices_next == V1_indices)
+    break;
+  end
+  V1_indices = V1_indices_next;
+  V2_indices = ~V1_indices;
+end
+
+
--- drake/examples/Pendulum/runValueIteration.m
+++ drake/examples/Pendulum/runValueIteration.m
@@ -1,13 +1,13 @@
-function runValueIteration
+function [J,PI,xbins]=runValueIteration
 
 plant = PendulumPlant;
 options.dt = 1e-2;
 options.gamma = .999;
 options.wrap_flag = [true;false];
 %cost = @mintime; ulimit = 1;
-cost = @lqrcost; ulimit = 2; % = 5 converges faster (but pumps less)
-xbins = {linspace(0,2*pi,51),linspace(-10,10,51)};
-ubins = linspace(-ulimit,ulimit,9);
+cost = @lqrcost; ulimit = 10; % = 5 converges faster (but pumps less)
+xbins = {linspace(0,2*pi,101),linspace(-10,10,101)};
+ubins = linspace(-ulimit,ulimit,51);
 mdp = MarkovDecisionProcess.discretizeSystem(plant,cost,xbins,ubins,options);
 
 function drawfun(J,PI)
@@ -18,13 +18,13 @@ function drawfun(J,PI)
   clf;
   n1=length(xbins{1});
   n2=length(xbins{2});
-  subplot(2,1,1);imagesc(xbins{1},xbins{2},reshape(ubins(PI),n1,n2)');
-  axis xy;
+  subplot(2,1,1);mesh(xbins{1},xbins{2},reshape(ubins(PI),n1,n2)');
+  axis xy;  axis tight;
   xlabel('theta');
   ylabel('thetadot');
   title('u(x)');
-  subplot(2,1,2);imagesc(xbins{1},xbins{2},reshape(J,n1,n2)');
-  axis xy;
+  subplot(2,1,2);mesh(xbins{1},xbins{2},reshape(J,n1,n2)');
+  axis xy;  axis tight;
   xlabel('theta');
   ylabel('thetadot');
   title('J(x)');
@@ -44,7 +44,7 @@ end
 
 function g = lqrcost(sys,x,u)
   xd = [pi;0];
-  g = (x-xd)'*(x-xd) + u^2;
+  g = (x-xd)'*(x-xd) + 10*u^2;
 end
 
 function g = mintime(sys,x,u)
